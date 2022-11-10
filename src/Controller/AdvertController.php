1599387d444d75e025ac295771b9cfd64600a69a
TRAN Juliette <tran.juliette.a@gmail.com>
2022-11-10 08:48:05 +0100

    Adding Search route

--- src/Controller/AdvertController.php
+++ src/Controller/AdvertController.php
@@ -19,10 +19,24 @@ class AdvertController extends AbstractController
     /**
      * @Route("/", name="app_advert_index", methods={ "GET" })
      */
-    public function index(AdvertRepository $advertRepository): Response
+    public function index(Request $request, AdvertRepository $advertRepository): Response
     {
         return $this->render('advert/index.html.twig', [
             'adverts' => $advertRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/search", name="app_advert_search", methods={ "GET" })
+     */
+    public function search(Request $request, AdvertRepository $advertRepository): Response
+    {
+        $title = $request->get("title", null);
+        $priceMin = $request->get("price-min", null);
+        $priceMax = $request->get("price-max", null);
+
+        return $this->render('advert/index.html.twig', [
+            'adverts' => $advertRepository->findBySearch($title, $priceMin, $priceMax)
         ]);
     }
 
@@ -60,7 +74,7 @@ class AdvertController extends AbstractController
 
     /**
      * @Entity("Advert", expr="repository.find(id)")
-     * @Route("/{id}/edit", name="app_advert_edit", methods={ "GET", "POST" })
+     * @Route("/{id}/edit", name="app_advert_edit", methods={ "GET", "PATCH" })
      */
     public function edit(Request $request, Advert $advert, AdvertRepository $advertRepository): Response
     {
@@ -81,17 +95,14 @@ class AdvertController extends AbstractController
 
     /**
      * @Entity("Advert", expr="repository.find(id)")
-     * @Route("/{id}", name="app_advert_delete", methods={ "POST" })
+     * @Route("/{id}", name="app_advert_delete", methods={ "DELETE" })
      */
     public function delete(Request $request, Advert $advert, AdvertRepository $advertRepository): Response
     {
-        /*
         if ($this->isCsrfTokenValid('delete'.$advert->getId(), $request->request->get('_token'))) {
-            $advertRepository->remove($advert, true);
+            $advert->setIsEnabled(false);
+            $advertRepository->save($advert, true);
         }
-        */
-        $advert->setIsEnabled(false);
-        $advertRepository->save($advert, true);
 
         return $this->redirectToRoute('app_advert_index', [], Response::HTTP_SEE_OTHER);
     }
